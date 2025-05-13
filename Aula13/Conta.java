ceb762bac1076bf1c792e89d13f28ac355a1a22e
Guilherme da Silva Fernandes Almeida <guilherme.almeida@gec.inatel.br>
2025-05-13 13:08:42 -0300

    Update Conta.java

--- Aula13/Conta.java
+++ Aula13/Conta.java
@@ -1,39 +1,35 @@
-package br.inatel.cdg;
-
 import java.util.*;
 
 public class Conta {
-	
-	private float saldo;
-	private float limite;
-	private Set<Cliente> clientes = new HashSet<>();
-	
-	
-	public Conta(float saldo, float limite) {
-		this.saldo = saldo;
-		this.limite = limite;
-	}
-	
-	public void addCliente(Cliente cliente) {
-		clientes.add(cliente);
-	}
-	
-	public void mostraInfo() {
-		System.out.println("Saldo: " + saldo);
-		System.out.println("Limite: " + limite);
-		
-		
-		for (Cliente cliente : clientes) {
-			try {
-				System.out.println("Nome do cliente: " + cliente.getNome());
-				System.out.println("CPF do cliente: " + cliente.getCpf());
-			} catch (NullPointerException e) {
-				System.out.println("Nao existe cliente nessa posicao");
-			}
-		}
-	}
-	
-	
-	
 
-}
+    private float saldo;
+    private float limite;
+    private Set<Cliente> clientes = new HashSet<>();
+
+    public Conta(float saldo, float limite) {
+        this.saldo = saldo;
+        this.limite = limite;
+    }
+
+    public void addCliente(Cliente cliente) {
+        try {
+            clientes.add(cliente);
+        } catch (Exception e) {
+            System.out.println("Erro ao adicionar cliente: " + e.getMessage());
+        }
+    }
+
+    public void mostraInfo() {
+        System.out.println("Saldo: " + saldo);
+        System.out.println("Limite: " + limite);
+
+        try {
+            for (Cliente cliente : clientes) {
+                System.out.println("Nome do cliente: " + cliente.getNome());
+                System.out.println("CPF do cliente: " + cliente.getCpf());
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao exibir informações dos clientes: " + e.getMessage());
+        }
+    }
+} 
