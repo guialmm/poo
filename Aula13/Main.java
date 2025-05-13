031c8d158769965cb0e76791143eab33047c907b
Guilherme da Silva Fernandes Almeida <guilherme.almeida@gec.inatel.br>
2025-05-13 13:08:32 -0300

    Update Main.java

--- Aula13/Main.java
+++ Aula13/Main.java
@@ -1,24 +1,19 @@
-package br.inatel.cdg;
-
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Cliente cliente1 = new Cliente("Tio", 4321);
-		Cliente cliente2 = new Cliente("Tia", 1234);
-		Cliente cliente3 = null;
-		
-		Conta conta = new Conta(1500, 500);
+    public static void main(String[] args) {
 
-		conta.addCliente(cliente1);
-		conta.addCliente(cliente2);
-		conta.addCliente(cliente3);
-		
-		conta.mostraInfo();
-		
-		
-		System.out.println("Fim da Main");
-		
-	}
+        Cliente cliente1 = new Cliente("Tio", 4321);
+        Cliente cliente2 = new Cliente("Tia", 1234);
+        Cliente cliente3 = null;
 
+        Conta conta = new Conta(1500, 500);
+
+        conta.addCliente(cliente1);
+        conta.addCliente(cliente2);
+        conta.addCliente(cliente3);
+
+        conta.mostraInfo();
+
+        System.out.println("Fim da Main");
+    }
 }
